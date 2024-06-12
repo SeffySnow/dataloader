dbc531cbf04174968f40a89e98b2734e65d70bd3
SeffySnow <haghighisepinood@gmail.com>
2024-06-12 18:28:30 -0400

    new

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -2,29 +2,26 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-class PSMDataset(Dataset):
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
     def __init__(self, csv_file):
-        self.data_frame = pd.read_csv(csv_file)
-        self.features = self.data_frame.iloc[:, :-1].values  # Assuming the last column is the label
-        self.labels = self.data_frame.iloc[:, -1].values
+        self.data = pd.read_csv(csv_file)
+        self.features = self.data.iloc[:, :-1].values  # All columns except the last one
+        self.labels = self.data.iloc[:, -1].values  # The last column
 
     def __len__(self):
-        return len(self.data_frame)
+        return len(self.data)
 
     def __getitem__(self, idx):
         features = torch.tensor(self.features[idx], dtype=torch.float32)
-        label = torch.tensor(self.labels[idx], dtype=torch.float32)
-        return features, label
+        labels = torch.tensor(self.labels[idx], dtype=torch.float32)
+        return features, labels
 
-def get_dataloader(csv_file, batch_size=32, shuffle=True, num_workers=2):
-    dataset = PSMDataset(csv_file)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    return dataloader
-
-if __name__ == "__main__":
-    # Example usage
-    train_loader = get_dataloader('path/to/train.csv', batch_size=32, shuffle=True)
-    test_loader = get_dataloader('path/to/test.csv', batch_size=32, shuffle=False)
-
-    for features, labels in train_loader:
-        print(features, labels)
+# Example usage:
+# train_dataset = CustomDataset('path/to/train.csv')
+# train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+# for features, labels in train_loader:
+#     print(features, labels)
